4aed3a9b418149bec2f73c42cacaa15a5178c7d1
RC0024904 <RC0024904@CRO-CABLAP1-16>
2017-09-14 15:22:12 +0100

    Says Hello World when the button is pressed

--- Hello World/Form1.Designer.cs
+++ Hello World/Form1.Designer.cs
@@ -37,8 +37,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 43);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Hello";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- Hello World/Form1.cs
+++ Hello World/Form1.cs
@@ -16,5 +16,10 @@ namespace Hello_World
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
